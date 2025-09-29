b6e1e4b81314cae900fea508d764ca004de45173
SilverDeveloper12 <167832190+SilverDeveloper12@users.noreply.github.com>
2025-09-29 20:13:26 -0300

    Add files via upload

---
+++ Aula002/src/Exemplo1.java
@@ -0,0 +1,13 @@
+
+public class Exemplo1 {
+    public static void main(String[] args){
+    System.out.println("Antes da repeticao.");
+    
+    for(int i = 0; i < 10; i++){
+        if(i % 2 == 0){
+        System.out.println("i: " + i);
+        }
+        }
+    System.out.println("Fora da repeticao...");
+  }
+}
